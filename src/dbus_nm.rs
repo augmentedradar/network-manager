14880d6f6d500a1fb886c9841d4338290f1ec8ea
Rani Glaser <raniglas@gmail.com>
2021-02-16 15:40:59 -0800

    disable adding connection interface to connection profile

--- src/dbus_nm.rs
+++ src/dbus_nm.rs
@@ -222,9 +222,11 @@ impl DBusNetworkManager {
         if let Ok(ssid_str) = access_point.ssid().as_str() {
             add_str(&mut connection, "id", ssid_str);
         }
+        /*
         if(conn_interface.chars().count() > 0) {
             add_str(&mut connection, "interface-name", conn_interface);
         }
+        */
         add_str(&mut connection, "type", "802-11-wireless");
         add_val(&mut connection, "autoconnect-priority", 5);
         settings.insert("connection".to_string(), connection);
